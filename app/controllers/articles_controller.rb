94368c7650dedb54d8e5011fb5cb09812333dba0
Mateus Esteves <mateus.esteves1994@gmail.com>
2018-04-09 21:33:18 -0300

    Seção 5.11 completa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,11 +6,15 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-
+    
     def show
         @article = Article.find(params[:id])
     end
-
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -19,9 +23,19 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
-
-    private
-        def article_params
-            params.require(:article).permit(:title, :text)
+    
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
         end
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
 end
